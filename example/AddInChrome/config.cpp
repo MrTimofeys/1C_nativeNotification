765d21ee1a4dbbbc7c592698347d12adc4758477
Timofey Melnikov <tmel@MacBook-Air-Timofey.local>
2026-01-18 23:32:38 +0300

    Удалил COM  и IE файлы - они не нужны

--- example/AddInChrome/config.cpp
+++ example/AddInChrome/config.cpp
@@ -6,9 +6,9 @@ AddInComponentType typeComponent = eAddInCom;
 #else
 AddInComponentType typeComponent = eAddInNative;
 #endif
-const char *nameFilePrj = ADDNCHROME OS ARCH EXESUFFIX;
+const char *nameFilePrj = AddInChrWin32.exe;
 #ifndef COMP_EXTERNAL
-const char *nameFileComponent = LIBPREFIX ADDNNATIVE OS ARCH ARCHSUFFIX "_" ADDNVER LIBSUFFIX;
+const char *nameFileComponent = AddInNative.dll;
 #else
 const char *nameFileComponent = ADDNNATIVE;
 #endif
